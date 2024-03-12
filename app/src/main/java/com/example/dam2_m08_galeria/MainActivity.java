15cb6b74becdb00cd65c70fee5ec6fb77bc20214
MatillaSan <118919845+MatillaSan@users.noreply.github.com>
2024-03-12 19:13:51 +0100

    Practica terminada

--- app/src/main/java/com/example/dam2_m08_galeria/MainActivity.java
+++ app/src/main/java/com/example/dam2_m08_galeria/MainActivity.java
@@ -5,36 +5,80 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+public class MainActivity extends AppCompatActivity {
     public static int RC_PHOTO_PICKER = 0;
-    ActivityResultLauncher<Intent> someActivityResultLauncher;
+    ActivityResultLauncher<Intent> galeryLauncher;
     ActivityResultLauncher<Intent> cameraLauncher;
-    @Override
+    ActivityResultLauncher<Intent> fullSizeLauncher;
+    String currentPhotoPath;
+    Uri photoURI;
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button boton = findViewById(R.id.button);
+        Button boton = findViewById(R.id.galeryButton);
+        Button cameraButton = findViewById(R.id.cameraButton);
+        Button fullSizeButton = findViewById(R.id.fullSizeButton);
+        ImageView imageView = findViewById(R.id.imageView);
 
         boton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openSomeActivityForResult(v);
+                openGalery(v);
             }
         });
 
-        someActivityResultLauncher = registerForActivityResult(
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+            }
+        });
+
+        fullSizeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File photoFile = null;
+                try {
+                    photoFile = createImageFile();
+                } catch (IOException ex) {
+                    // Error occurred while creating the File
+                }
+                // Continue only if the File was successfully created
+                if (photoFile != null) {
+                    photoURI = FileProvider.getUriForFile(MainActivity.this,
+                            "com.example.android.fileprovider",
+                            photoFile);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                }
+                fullSizeLauncher.launch(intent);
+            }
+        });
+
+        galeryLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>() {
                 @Override
@@ -43,7 +87,6 @@ public class MainActivity extends AppCompatActivity {
                         // There are no request codes
                         Intent data = result.getData();
                         Uri uri = data.getData();
-                        ImageView imageView = findViewById(R.id.imageView);
                         imageView.setImageURI(uri);
                     }
                 }
@@ -67,16 +110,51 @@ public class MainActivity extends AppCompatActivity {
             }
         );
 
-        cameraLauncher.launch(new Intent(MediaStore.ACTION_IMAGE_CAPTURE));
+        fullSizeLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    try {
+                        ContentResolver resolver = getContentResolver();
+                        InputStream inputStream = resolver.openInputStream(photoURI);
+                        Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                        Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, 300, 300, false);
+                        imageView.setImageBitmap(scaledBitmap);
+                    } catch (FileNotFoundException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        );
     }
 
-    public void openSomeActivityForResult(View view) {
+    public void openCamera() {
+            cameraLauncher.launch(new Intent(MediaStore.ACTION_IMAGE_CAPTURE));
+    }
 
+    public void openGalery(View view) {
         //Create Intent
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType("image/jpg");
         intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
         //Launch activity to get result
-        someActivityResultLauncher.launch(intent);
+        galeryLauncher.launch(intent);
+    }
+
+    public File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
